df94ada9e4a10d759cc2df3ceccf6eef53eab803
Lucas de Pace <38995017+lucas-pace@users.noreply.github.com>
2023-12-13 01:39:44 -0300

    Update colar-no-console.js

--- colar-no-console.js
+++ colar-no-console.js
@@ -12,51 +12,8 @@
 */
 
 
-const baseUrl = 'https://0l51a8xu73.execute-api.eu-west-1.amazonaws.com/production/votes';
-const eventCode = 'MENCLUB2023';
-const referer = 'https://vote.globesoccer.com/';
 
-
-function makeRequest() {
-  return new Promise(resolve => {
-    const id = generateUUID();
-    console.log('UUID gerado:', id);
-
-    fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'authority': '0l51a8xu73.execute-api.eu-west-1.amazonaws.com',
-        'accept': '*/*',
-        'accept-language': 'pt-BR,pt;q=0.9',
-        'origin': 'https://vote.globesoccer.com',
-        'referer': referer,
-        'sec-ch-ua': '"Chromium";v="118", "Opera";v="104", "Not=A?Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0'
-      },
-      body: `eventCode=${eventCode}&voterId=${id}&hash=&vote=9`
-    })
-    .then(response => {
-      console.log('Response:', response.status);
-      resolve(); // Resolve the promise when the request is completed
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      resolve(); // Resolve the promise even if there's an error
-    });
-  });
-}
-
-async function sendRequests() {
-  while(true){
-    await makeRequest();
-  }
-}
+const tempo = 500 // Envio de voto a cada 500ms ( meio segundo ). Alterar se quiser, mas pode crashar o navegador. Se tiver travando muito, aumenta o tempo
 
 function generateUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -66,5 +23,46 @@ function generateUUID() {
   });
 }
 
-// Start sending requests
-sendRequests();
+
+const headers = new Headers();
+  headers.append("authority", "gs-voting.oddb.co");
+  headers.append("accept", "*/*");
+  headers.append("accept-language", "pt-BR,pt;q=0.9");
+  headers.append("origin", "https://vote.globesoccer.com");
+  headers.append("referer", "https://vote.globesoccer.com/");
+  headers.append("sec-ch-ua", "\"Opera\";v=\"105\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"");
+  headers.append("sec-ch-ua-mobile", "?1");
+  headers.append("sec-ch-ua-platform", "\"Android\"");
+  headers.append("sec-fetch-dest", "empty");
+  headers.append("sec-fetch-mode", "cors");
+  headers.append("sec-fetch-site", "cross-site");
+  headers.append("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36");
+
+
+function enviarSolicitacao() {
+  
+
+  var formdata = new FormData();
+  formdata.append("eventCode", "FINALMENCLUB2023");
+  formdata.append("voterId", generateUUID());
+  formdata.append("hash", "");
+  formdata.append("vote", "5");
+
+  var requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  fetch("https://gs-voting.oddb.co/votes", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+
+// Inicia o loop de envio da solicitação POST a cada segundo
+var intervalId = setInterval(enviarSolicitacao, tempo);
+
+
